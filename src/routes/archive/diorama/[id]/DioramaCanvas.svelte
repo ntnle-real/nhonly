8f88a36e50357ee5fb66c28f9199a3aca2f1c642
n <n@.com>
2026-04-07 10:04:10 -0400

    feat(03-02-living-diorama): create DioramaCanvas.svelte component with Three.js integration

---
+++ src/routes/archive/diorama/[id]/DioramaCanvas.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { onMount, onDestroy } from 'svelte';
+	import { initScene, type SceneHandle } from '$lib/diorama.scene';
+
+	let canvasEl: HTMLCanvasElement;
+	let sceneHandle: SceneHandle | null = null;
+
+	onMount(() => {
+		if (canvasEl) {
+			sceneHandle = initScene(canvasEl);
+		}
+	});
+
+	onDestroy(() => {
+		sceneHandle?.dispose();
+		sceneHandle = null;
+	});
+</script>
+
+<canvas
+	bind:this={canvasEl}
+	style="
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 0;
+    pointer-events: none;
+  "
+></canvas>
